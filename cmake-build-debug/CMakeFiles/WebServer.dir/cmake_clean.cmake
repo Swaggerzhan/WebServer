abc81884d085eff168c13e2997e8ebab4838edcc
志一 <ebai98@qq.com>
2021-04-06 20:34:17 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/WebServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebServer.dir/main.cpp.o"
+  "WebServer"
+  "WebServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
